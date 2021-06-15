67ca92393989183e5be2d94e3429ec2e80108d26
nterw <n.terwort97@gmail.com>
2021-06-15 07:08:54 +0200

    awesome

---
+++ .config/awesome/scripts/wifi-connect.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+internet_connection=false;
+connection_string='inet 192'
+while [ "$internet_connection" = false ];
+do
+	router=$(nmcli dev wifi | head -2 | tail -1 | cut -c 28- | cut -c -16);
+	nmcli con up id $router
+
+	test_connection=$(ifconfig | grep 'inet 192')
+	if (grep -q "$connection_string" <<< "$test_connection"); then
+		break;
+	fi
+done
+nm_pid=$(pgrep nm-applet);
+pkill -g $nm_pid;
+
+
+styli.sh -s abstract;
