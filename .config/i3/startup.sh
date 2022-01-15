ffe6e776d9cf664a93feccf511fb8e0a5f2503b6
NiTE97 <n.terwort97@gmail.com>
2022-01-15 11:21:58 +0100

    Minor changes

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+#Keyboard Layout
 setxkbmap -layout de
-heroic
+#Screensaver disable
 xset s off
 xset -dpms
 xset s noblank
+
+#Monitor config
+xrandr --auto
+xrandr --output DP-5 --mode 1920x1080 --rate 60 --pos 0x1260
+xrandr --output DP-2 --mode 2560x1440 --rate 165 --pos 1920x1080 --primary
+xrandr --output HDMI-0 --mode 1920x1080 --rate 60 --pos 2240x0
+
+#Autostart Apps
+picom &
+~/.config/polybar/launch.sh &
+nitrogen --restore &
+ckb-next -b &
+heroic &
+dunst &
+
